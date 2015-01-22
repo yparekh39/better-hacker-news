b1363aa8497e8d1cbfce0711b1525425d06cf4f9
Yash Parekh <yparekh39@gmail.com>
2015-01-22 16:44:00 -0600

    Added node, removed uneccesary node modules

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-(function(){
 var betterHackerNewsApp = angular.module('betterHackerNewsApp', []);
-betterHackerNewsApp.controller('BetterHNApp', function(){
-
+var cheerio = require('cheerio');
+var request = require('request');
+var url = 'http://news.ycombinator.com';
+var text2;
+request(url, function(err, resp, body){
+	if(err)
+		throw err;
+	$ = cheerio.load(body);
+	$('.title a').each(function() {
+		text2 = $(this).text();
+		console.log($(this).text()+"\n");
+		console.log($(this).attr("href")+"\n");
+	});
 });
-var links[] = {};
-})();
+betterHackerNewsApp.controller("betterHNController", function($scope){
+	$scope.test = text2;
+});
