fb57d68dee88f332f4c7ae9eeac029b4e9b089b6
Yash Parekh <yparekh39@gmail.com>
2015-02-21 17:30:32 -0600

    Hacker news now uses Firebase to host the data, successfully retrieved top thirty links from hacker news firebase

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-var betterHackerNewsApp = angular.module('betterHackerNewsApp', []);
-var cheerio = require('cheerio');
-var request = require('request');
-var url = 'http://news.ycombinator.com';
-var text2;
-request(url, function(err, resp, body){
-	if(err)
-		throw err;
-	$ = cheerio.load(body);
-	$('.title a').each(function() {
-		text2 = $(this).text();
-		console.log($(this).text()+"\n");
-		console.log($(this).attr("href")+"\n");
-	});
-});
-betterHackerNewsApp.controller("betterHNController", function($scope){
-	$scope.test = text2;
-});
+var betterHackerNewsApp = angular.module("betterHackerNewsApp", ["firebase"]);
+
+betterHackerNewsApp.controller("HackerNewsCtrl", ["$scope", "$firebase", 
+	function($scope, $firebase) {
+  	// now we can use $firebase to synchronize data between clients and the server!
+		var topStoriesRef = new Firebase("https://hacker-news.firebaseio.com/v0/topstories");
+ 	 	var syncTop = $firebase(topStoriesRef);
+  		var topStories = syncTop.$asArray(); //JSON Object of the IDs for the top 100 stories
+  		var test;
+  		var items = [];
+  		topStories.$loaded().then(function(){
+  			//test = topStories[0].$value;
+  			//console.log(test);	
+  			for(var i = 0; i < 30; i++){
+  				//console.log("https://hacker-news.firebaseio.com/v0/item/" + topStories[i].$value);
+  				var storyRef = new Firebase("https://hacker-news.firebaseio.com/v0/item/" + topStories[i].$value);
+  				//console.log(storyRef);
+  				var syncStory = $firebase(storyRef);
+  				var story = syncStory.$asObject();
+  				//console.log(story);
+  				items.push(story);//Adds story objects to items
+  			}
+  			console.log(items);
+  			$scope.stories = items;
+  		}).catch(function(error){
+  			console.log("Error:", error);
+  		});
+  	 		
+  		
+	}
+]);
